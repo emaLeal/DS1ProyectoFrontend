2a6ead64d96adea4b9ebd7c4152511e0059a50ab
HDGM <garcia.hernan@correounivalle.edu.co>
2025-05-03 21:08:56 -0500

    Eliminar todo el contenido de la rama

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './authentication/login/login.component';
-
-export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: 'login', pathMatch: 'full' } // Redirige a login por defecto
-];
--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-import { LoginComponent } from './app/authentication/login/login.component'; // ✅ Importa LoginComponent
-
-bootstrapApplication(AppComponent, {
-  ...appConfig,
-  providers: [...(appConfig.providers || []), LoginComponent], // ✅ Agrega LoginComponent aquí
-}).catch((err) => console.error(err));
