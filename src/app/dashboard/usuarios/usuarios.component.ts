978beec0edd29b91f4f312731fd16c34a1180ddf
HDGM <garcia.hernan@correounivalle.edu.co>
2025-05-12 22:05:58 -0500

    front version 1.1.3

--- src/app/dashboard/usuarios/usuarios.component.ts
+++ src/app/dashboard/usuarios/usuarios.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { NgbModalModule, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-usuarios',
