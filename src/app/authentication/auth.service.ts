3bdbc79d3a354513ddcb4170894a36aeb76b9c37
emaLeal <emanuel.leal@correounivalle.edu.co>
2025-06-16 17:28:29 -0500

    Correcciones

--- src/app/authentication/auth.service.ts
+++ src/app/authentication/auth.service.ts
@@ -1,27 +1,16 @@
-<<<<<<< HEAD
-import { HttpClient } from '@angular/common/http';
-=======
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
->>>>>>> 7d23f567d5cd6d0829fa751721be5398cea4fbb0
 import { Injectable } from '@angular/core';
 import { Login, User } from './auth.types'
 import { Router } from '@angular/router';
 import { environment } from '../../../public/environment';
-<<<<<<< HEAD
-=======
 import { catchError } from 'rxjs/operators';
 import { Observable, throwError } from 'rxjs';
->>>>>>> 7d23f567d5cd6d0829fa751721be5398cea4fbb0
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private auth: boolean = false;
-<<<<<<< HEAD
-  constructor(private httpClient: HttpClient, private router: Router
-  ) { }
-=======
 
   constructor(private httpClient: HttpClient, private router: Router) { }
 
@@ -43,35 +32,11 @@ export class AuthService {
     }
     return throwError(() => errorMessage);
   }
->>>>>>> 7d23f567d5cd6d0829fa751721be5398cea4fbb0
 
   login(login: Login) {
     const url: string = environment.baseUrl + environment.authentication.login;
     return this.httpClient
       .post(url, login)
-<<<<<<< HEAD
-      .subscribe((value: any) => {
-        const token = value;
-        const JSONToken = JSON.stringify(value)
-        localStorage.setItem('token', JSONToken);
-        this.auth = true;
-        let urlProfile: string = environment.baseUrl + environment.authentication.profile
-        return this.httpClient.get(urlProfile, { headers: { 'authorization': `Bearer ${token.access}` } }).subscribe(user => {
-          localStorage.setItem('user_data', JSON.stringify(user))
-          this.router.navigate(['/dashboard']);
-        })
-      });
-  }
-
-  register(userForm: User) {
-    const url: string = environment.baseUrl + environment.authentication.register;
-    return this.httpClient
-      .post(url, userForm)
-      .subscribe((value) => {
-        alert("Te has registrado correctamente")
-        this.router.navigate(['/login']);
-      });
-=======
       .pipe(
         catchError(this.handleError)
       );
@@ -84,17 +49,12 @@ export class AuthService {
       .pipe(
         catchError(this.handleError)
       );
->>>>>>> 7d23f567d5cd6d0829fa751721be5398cea4fbb0
   }
 
   get getProfile() {
     return JSON.parse(localStorage.getItem('user_data')!)
   }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 7d23f567d5cd6d0829fa751721be5398cea4fbb0
   get isAuthenticated(): boolean {
     return this.auth;
   }
--- src/app/authentication/auth.types.ts
+++ src/app/authentication/auth.types.ts
@@ -1,27 +1,11 @@
 export interface Login {
-<<<<<<< HEAD
-    document_id: number,
-=======
     document_id: string,
->>>>>>> 7d23f567d5cd6d0829fa751721be5398cea4fbb0
     password: string
 }
 
 export interface User {
     name: string,
     last_name: string,
-<<<<<<< HEAD
-    document_id: number,
-    address: string,
-    role: string,
-    phone: number,
-    cell_phone: number,
-    birth_date: Date,
-    gender:string,
-    identification_type:string,
-    email:string
-
-=======
     document_id: string,
     address: string,
     phone: string,
@@ -31,5 +15,4 @@ export interface User {
     identification_type: string,
     email: string,
     password: string
->>>>>>> 7d23f567d5cd6d0829fa751721be5398cea4fbb0
 }
